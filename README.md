ba4993e79170beee49a12a155408da0290741f23
Abhimanyu R B <abhimanyurbsa@gmail.com>
2024-01-05 14:14:23 +0530

    App file added -> patient doctype

---
+++ README.md
@@ -0,0 +1,7 @@
+## Mysite
+
+newapp
+
+#### License
+
+MIT
---
+++ mysite/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Mysite",
+			"type": "module",
+			"label": _("Mysite")
+		}
+	]
