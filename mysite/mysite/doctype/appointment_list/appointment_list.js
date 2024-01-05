42823e3fe385907ebb29748100a8442310b8cfc5
Abhimanyu R B <abhimanyurbsa@gmail.com>
2024-01-05 21:32:29 +0530

    appointment update

--- mysite/mysite/doctype/appointment_list/appointment_list.js
+++ mysite/mysite/doctype/appointment_list/appointment_list.js
@@ -5,4 +5,12 @@ frappe.ui.form.on('Appointment List', {
 	// refresh: function(frm) {
 
 	// }
+	no_of_visits: frm => {
+		let visits = frm.doc.no_of_visits,
+			total_visits = no_of_visits + 1
+
+		frm.set_value('no_of_visits', total_visits)
+		frm.refresh_field('no_of_visits')
+	}
+
 });
